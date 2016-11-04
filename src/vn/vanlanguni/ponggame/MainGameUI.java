25f689d32a23c6813b95dfc9e04348aff3f37712
sonvinh <sonvinh06@gmail.com>
2016-11-04 13:49:09 +0700

    Add BallDeltaX on line 150 and 171

--- src/vn/vanlanguni/ponggame/MainGameUI.java
+++ src/vn/vanlanguni/ponggame/MainGameUI.java
@@ -20,7 +20,7 @@ public class MainGameUI extends JFrame{
 	public MainGameUI(){
 		setPreferredSize(new Dimension(_WIDTH, _HEIGHT));
 		setLayout(new BorderLayout());
-		setTitle("Pong Game - K21T Ltd.");
+		setTitle("Pong Game - K21Tv0.5 Ltd.");
 		pongPanel = new PongPanel();
 		getContentPane().add(pongPanel, BorderLayout.CENTER);
 		pack();
